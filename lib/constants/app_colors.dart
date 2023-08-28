4ef1330fe34c7a603dc6f7b83265e95392a0907e
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-29 04:48:01 +0500

    mini

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/animation.dart';
+
+class AppDarkColors {
+  static Color black100 = const Color(0xff1B262E);
+  static Color black90 = const Color(0xff354349);
+  static Color black60 = const Color(0xff606D76);
+  static Color black45 = const Color(0xffA9B4BC);
+  static Color black20 = const Color(0xffC5CDD2);
+  static Color black10 = const Color(0xffE7ECF0);
+  static Color black1 = const Color(0xffF8F9FB);
+}
+
+class AppColors {
+  static Color blue = const Color(0xff2A4BA0);
+  static Color blueDark = const Color(0xff153075);
+  static Color orange = const Color(0xffF9B023);
+  static Color orangeLite = const Color(0xffFFC83A);
+}
