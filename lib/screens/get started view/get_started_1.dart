a7aef818385ff0a8ee7ad9ab38cd9b847dc6aa1f
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-30 02:41:14 +0500

    grocery

--- lib/screens/get started view/get_started_1.dart
+++ lib/screens/get started view/get_started_1.dart
@@ -20,75 +20,77 @@ class _GetStartedViewState extends State<GetStartedView> {
 
     return Scaffold(
       backgroundColor: AppColors.blue,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:
-              const EdgeInsets.only(left: 30, right: 30, top: 40, bottom: 20),
-          child: Column(
-            children: [
-              Container(
-                // color: Colors.red,
-                child: SizedBox(
-                  height: height * 0.35,
-                  child: PageView(
-                    controller: pageController,
-                    onPageChanged: (index) {
-                      setState(() {
-                        activePage = index;
-                      });
-                    },
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            'Your holiday shopping delivered to Screen one',
-                            style: CustomTextStyle30.h1Bold30,
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          Text(
-                            'There\'s something for everyone to enjoy with Sweet Shop Favourites Screen 1',
-                            style: CustomTextStyle18.h1Medium18,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            'Your holiday shopping delivered to Screen two',
-                            style: CustomTextStyle30.h1Bold30,
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          Text(
-                            'There\'s something for everyone to enjoy with Sweet Shop Favourites Screen 2',
-                            style: CustomTextStyle18.h1Medium18,
-                          ),
-                        ],
-                      ),
-                    ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 30, right: 30, top: 40, bottom: 20),
+            child: Column(
+              children: [
+                Container(
+                  // color: Colors.red,
+                  child: SizedBox(
+                    height: height * 0.35,
+                    child: PageView(
+                      controller: pageController,
+                      onPageChanged: (index) {
+                        setState(() {
+                          activePage = index;
+                        });
+                      },
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'Your holiday shopping delivered to Screen one',
+                              style: CustomTextStyle30.h1Bold30,
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Text(
+                              'There\'s something for everyone to enjoy with Sweet Shop Favourites Screen 1',
+                              style: CustomTextStyle18.h1Medium18,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Your holiday shopping delivered to Screen two',
+                              style: CustomTextStyle30.h1Bold30,
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Text(
+                              'There\'s something for everyone to enjoy with Sweet Shop Favourites Screen 2',
+                              style: CustomTextStyle18.h1Medium18,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Row(
-                children: List.generate(
-                    2,
-                    (index) => Padding(
-                          padding: const EdgeInsets.all(3),
-                          child: Container(
-                            height: activePage == index ? 5 : 7,
-                            width: activePage == index ? 25 : 45,
-                            decoration: BoxDecoration(
-                                color: activePage == index
-                                    ? AppDarkColors.black45
-                                    : AppDarkColors.black1,
-                                borderRadius: BorderRadius.circular(2)),
-                          ),
-                        )),
-              ),
-            ],
+                Row(
+                  children: List.generate(
+                      2,
+                      (index) => Padding(
+                            padding: const EdgeInsets.all(3),
+                            child: Container(
+                              height: activePage == index ? 5 : 7,
+                              width: activePage == index ? 25 : 45,
+                              decoration: BoxDecoration(
+                                  color: activePage == index
+                                      ? AppDarkColors.black45
+                                      : AppDarkColors.black1,
+                                  borderRadius: BorderRadius.circular(2)),
+                            ),
+                          )),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/custom button/custom_button.dart
+++ lib/widgets/custom button/custom_button.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_order_app_flutter/constants/app_colors.dart';
 
 class CustomButton extends StatelessWidget {
   final String buttonText;
-  const CustomButton({super.key, required this.buttonText});
+  final Color? buttonColor;
+  const CustomButton({super.key, required this.buttonText, this.buttonColor});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {},
-      style: ElevatedButton.styleFrom(),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor ?? AppColors.blue,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: BorderSide(color: AppColors.blue)),
+      ),
       child: Text(buttonText),
     );
   }
