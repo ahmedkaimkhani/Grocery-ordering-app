bd6969b4fe914bb132ffaca71d98ae64a77e70e3
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-29 06:03:18 +0500

    mini app

---
+++ lib/screens/get started view/get_started_1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_order_app_flutter/constants/app_colors.dart';
+
+class GetStartedView extends StatelessWidget {
+  const GetStartedView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.blue,
+    );
+  }
+}
