6b00c2fd78c1d69eb98cb71b1e5881327552fdf1
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-29 05:41:36 +0500

    grocery

---
+++ lib/constants/app_fonts.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class AppFonts {
+  static TextStyle rubikMedium = const TextStyle(fontFamily: 'Rubik Medium');
+  static TextStyle rubikRegular = const TextStyle(fontFamily: 'Rubik Regular');
+}
