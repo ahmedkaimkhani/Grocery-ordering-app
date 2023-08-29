530802e70657fcfd5acbdf6c60eeab95c7b935b1
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-29 18:24:20 +0500

    Grocery

--- lib/constants/custom_textstyle.dart
+++ lib/constants/custom_textstyle.dart
@@ -1,18 +1,165 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_order_app_flutter/constants/app_colors.dart';
 
-class FontStyle {
-  static final font_30 = TextStyle(fontSize: 30);
-  static final font_26 = TextStyle(fontSize: 26);
-  static final font_20 = TextStyle(fontSize: 20);
-  static final font_18 = TextStyle(fontSize: 18);
-  static final font_16 = TextStyle(fontSize: 16);
-  static final font_14 = TextStyle(fontSize: 14);
-  static final font_12 = TextStyle(fontSize: 12);
-}
+class CustomTextStyles {
+  static TextStyle h1Bold30 = const TextStyle(
+      // For 30 Size
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+      color: Color(0xffFAFBFD),
+      fontFamily: 'Rubik Medium');
 
-class CustomFontWeight {
-  static final weight_700 = TextStyle(fontWeight: FontWeight.w700);
-  static final weight_600 = TextStyle(fontWeight: FontWeight.w600);
-  static final weight_500 = TextStyle(fontWeight: FontWeight.w500);
-  static final weight_400 = TextStyle(fontWeight: FontWeight.w400);
+  static TextStyle h1SemiBold30 = TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.w600,
+      color: AppDarkColors.black100,
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1Medium30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w500,
+    color: AppDarkColors.black100,
+  );
+
+  static TextStyle h1Regular30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    color: AppDarkColors.black100,
+  );
+
+  // for 26 size
+  static TextStyle h1Bold26 = TextStyle(
+      fontSize: 26,
+      fontWeight: FontWeight.bold,
+      color: AppDarkColors.black100,
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1SemiBold26 = TextStyle(
+      fontSize: 26,
+      fontWeight: FontWeight.w600,
+      color: AppDarkColors.black100,
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1Medium26 = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w500,
+    color: AppDarkColors.black100,
+  );
+
+  static TextStyle h1Regular26 = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.normal,
+    color: AppDarkColors.black100,
+  );
+
+  // for 22 size
+  static TextStyle h1SemiBold22 = const TextStyle(
+      fontSize: 26,
+      fontWeight: FontWeight.w600,
+      color: Color(0xffF8F9FB),
+      fontFamily: 'Rubik Medium');
+
+  // for 20 size
+  static TextStyle h1Bold20 = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: AppDarkColors.black100,
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1SemiBold20 = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      color: AppDarkColors.black100,
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1Medium20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: AppDarkColors.black100,
+  );
+
+  static TextStyle h1Regular20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    color: AppDarkColors.black100,
+  );
+
+  // For 18 size
+  static TextStyle h1Bold318 = const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      color: Color(0xff1E222B),
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1SemiBold18 = TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: AppDarkColors.black100,
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1Medium18 = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: Color(0xffB2BBCE),
+  );
+
+  static TextStyle h1Regular18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+    color: AppDarkColors.black100,
+  );
+
+  // For 16 size
+
+  static TextStyle h1SemiBold16 = const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: Color(0xff1E222B),
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1Medium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppDarkColors.black100,
+  );
+
+  static TextStyle h1Regular16 = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Color(0xff616A7D),
+  );
+
+  // For 14 size
+
+  static TextStyle h1SemiBold14 = const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: Color(0xff1E222B),
+      fontFamily: 'Rubik Medium');
+
+  static TextStyle h1Medium14 = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Color(0xff1E222B),
+  );
+
+  static TextStyle h1Regular14 = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: Color(0xff8891A5),
+  );
+
+  // For 12 size
+
+  static TextStyle h1Medium12 = const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: Color(0xff8891A5),
+  );
+
+  static TextStyle h1Regular12 = const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    color: Color(0xff616A7D),
+  );
 }
