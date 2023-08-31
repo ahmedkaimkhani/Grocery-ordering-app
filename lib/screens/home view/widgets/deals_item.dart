6fd033018478b3207436f8371e2f5308cba1d7ea
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-31 15:17:11 +0500

    Grocery

--- lib/screens/home view/widgets/deals_item.dart
+++ lib/screens/home view/widgets/deals_item.dart
@@ -4,13 +4,13 @@ final List<Map<String, dynamic>> dealItems = [
   {
     'price': '\$325',
     'name': 'Orange Package 1 | 1 bundle',
-    'image': 'https://www.pexels.com/photo/orange-fruit-161559/'
+    'image':
+        'https://www.wallpaperflare.com/static/583/448/1015/oranges-basket-fruit-leaves-wallpaper.jpg'
   },
   {
     'price': '\$89',
     'name': 'Green Tea Package 2 | 1 bundle',
-    'image':
-        'https://www.freepik.com/premium-vector/tea-ad-background-realistic-green-black-drink-advertisement-with-branded-pyramid-teabags-green-leaves-porcelain-cup-blurred-background-hot-beverage-banner-design-vector-illustration_61087649.htm#query=green%20tea&position=7&from_view=keyword&track=ais'
+    'image': 'https://img.lovepik.com/desgin_photo/45003/1444_list.jpg'
   }
 ];
 
@@ -19,34 +19,31 @@ class DealsItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GridView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
-          itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: dealItems.length,
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            children: [
+              ClipRRect(
+                child: Image.network(
+                  '${dealItems.elementAt(index)['image']}',
+                  height: 150,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
-              child: Column(
-                children: [
-                  ClipRRect(
-                    child: Image.network(
-                      '${dealItems.elementAt(index)['image']}',
-                      height: 150,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        )
-      ],
+            ],
+          ),
+        );
+      },
     );
   }
 }
