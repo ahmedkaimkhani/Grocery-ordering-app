d17206b30b6c295b42599dea43120886b3eb6337
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-02 04:32:13 +0500

    Grocery

--- lib/screens/categories view/categories_view.dart
+++ lib/screens/categories view/categories_view.dart
@@ -1,42 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_order_app_flutter/constants/custom_textstyle.dart';
 import 'package:grocery_order_app_flutter/constants/string.dart';
+import 'package:grocery_order_app_flutter/screens/categories%20view/model/shops_products.dart';
 
 import '../../constants/app_colors.dart';
 
-class CategoriesView extends StatelessWidget {
+class CategoriesView extends StatefulWidget {
   const CategoriesView({super.key});
 
   @override
+  State<CategoriesView> createState() => _CategoriesViewState();
+}
+
+class _CategoriesViewState extends State<CategoriesView> {
+  late String selectedCategory;
+  late Map<String, dynamic> selectedShop;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-            height: 155,
-            width: double.infinity,
-            color: AppColors.blue,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              height: 155,
+              width: double.infinity,
+              color: AppColors.blue,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppText.categoryPageText1,
+                      style: CustomTextStyle45.h1Regular50,
+                    ),
+                    Text(
+                      AppText.categoryPageText2,
+                      style: CustomTextStyle45.h1Bold50,
+                    )
+                  ],
+                ),
+              )),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
             child: Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-                right: 20,
+              padding: const EdgeInsets.only(left: 20, top: 20),
+              child: Row(
+                children: data
+                    .map((categoryData) => FilterChip(
+                          showCheckmark: false,
+                          selectedColor: AppColors.orange,
+                          label: Text(categoryData['category']),
+                          onSelected: (value) {},
+                        ))
+                    .toList(),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    AppText.categoryPageText1,
-                    style: CustomTextStyle45.h1Regular50,
-                  ),
-                  Text(
-                    AppText.categoryPageText2,
-                    style: CustomTextStyle45.h1Bold50,
-                  )
-                ],
-              ),
-            )),
-      ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
