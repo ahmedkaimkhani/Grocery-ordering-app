9217d065e7ca0e043491b24142566263727af236
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-02 01:10:09 +0500

    Grocery

---
+++ lib/screens/categories view/model/shops_products.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> data = [
+  // Index 0
+  {
+    // Chip 1
+    'category': 'Meats & Fishes',
+    'shops': [
+      // Shop 1 of Meants $ Fishes
+      {
+        'shopname': 'Big & Small Fishes',
+        'price': '\$36/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {
+            'productname': 'Clown Tang.H03',
+            'price': '\$325',
+            'image': 'imageURL'
+          },
+          {
+            'productname': 'Clownfish.H03',
+            'price': '\$89',
+            'image': 'imageURL'
+          },
+        ]
+      },
+      // Shop 2 of Meants $ Fishes
+      {
+        'shopname': 'Halal Meats',
+        'price': '\$90/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {'productname': 'Beef Steak', 'price': '\$325', 'image': 'imageURL'},
+          {'productname': 'Quail Meat', 'price': '\$150', 'image': 'imageURL'},
+        ]
+      },
+      // Shop 3 of Meants $ Fishes
+      {
+        'shopname': 'Seafood Sensations',
+        'price': '\$200/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {
+            'productname': 'Salmon Fillet',
+            'price': '\$325',
+            'image': 'imageURL'
+          },
+          {'productname': 'Swordfish', 'price': '\$89', 'image': 'imageURL'},
+        ]
+      }
+    ]
+  },
+  // Index 1
+  {
+    // Chip 2
+    'category': 'Vegetables',
+    'shops': [
+      // Shop 1 of Vegetables
+      {
+        'shopname': 'Garden of Eatables',
+        'price': '\$36/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {'productname': 'Tomatoes', 'price': '\$20', 'image': 'imageURL'},
+          {'productname': 'Cabbage', 'price': '\$45', 'image': 'imageURL'},
+        ]
+      },
+      // Shop 2 of Vegetables
+      {
+        'shopname': 'Fresh Harvest Market',
+        'price': '\$90/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {'productname': 'Potatoes', 'price': '\$15', 'image': 'imageURL'},
+          {'productname': 'Carrots', 'price': '\$32', 'image': 'imageURL'},
+        ]
+      },
+      // Shop 3 of Vegetables
+      {
+        'shopname': 'The Veggie Patch',
+        'price': '\$36/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {'productname': 'Onions', 'price': '\$325', 'image': 'imageURL'},
+          {'productname': 'Mushrooms', 'price': '\$89', 'image': 'imageURL'},
+        ]
+      }
+    ]
+  },
+  // Index 2
+  {
+    // Chip 3
+    'category': 'Fruits',
+    'shops': [
+      // Shop 1 of Fruits
+      {
+        'shopname': 'Juicy Delights Fruit',
+        'price': '\$31/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {'productname': 'Apples', 'price': '\$48', 'image': 'imageURL'},
+          {'productname': 'Bananas', 'price': '\$31', 'image': 'imageURL'},
+        ]
+      },
+      // Shop 2 of Fruits
+      {
+        'shopname': 'Pomegranate Palace',
+        'price': '\$35/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {'productname': 'Oranges', 'price': '\$35', 'image': 'imageURL'},
+          {'productname': 'Grapes', 'price': '\$43', 'image': 'imageURL'},
+        ]
+      },
+      // Shop 3 of Fruits
+      {
+        'shopname': 'Exotic Fruit Oasis',
+        'price': '\$61/KG',
+        'image': 'imageURL',
+        // Shops Products
+        'product': [
+          {'productname': 'Strawberries', 'price': '\$61', 'image': 'imageURL'},
+          {'productname': 'Mangoes', 'price': '\$90', 'image': 'imageURL'},
+        ]
+      }
+    ]
+  },
+];
