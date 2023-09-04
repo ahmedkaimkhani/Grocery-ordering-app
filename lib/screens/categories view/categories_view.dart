bde7b80f4e31e78b7d2f5fee333b9e1269f2993e
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-04 16:43:22 +0500

    Grocery

--- lib/screens/categories view/categories_view.dart
+++ lib/screens/categories view/categories_view.dart
@@ -81,14 +81,12 @@ class _CategoriesViewState extends State<CategoriesView> {
           if (selectedCategory != null)
             Padding(
               padding: const EdgeInsets.only(left: 20, right: 20),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 10),
-                    // Display shops in the selected category
-                    _buildShopList(selectedCategory!),
-                  ],
-                ),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  // Display shops in the selected category
+                  _buildShopList(selectedCategory!),
+                ],
               ),
             )
         ],
