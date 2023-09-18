1b2298a12e4267fb885c777d37c6d59b109dc42e
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-19 03:46:55 +0500

    Grocery

--- lib/screens/add new cards/add_cards.dart
+++ lib/screens/add new cards/add_cards.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_order_app_flutter/constants/app_colors.dart';
 import 'package:grocery_order_app_flutter/widgets/custom_appbar.dart';
 
+import '../../constants/custom_textstyle.dart';
+import '../../widgets/custom button/custom_button.dart';
+import '../cart item/cart.list.dart';
 import '../checkout view/widgets/custom_textfield.dart';
 
 class AddCard extends StatelessWidget {
   const AddCard({super.key});
 
+  // parse price to covert String to double
+  double parsePrice(String price) {
+    return double.parse(price.replaceAll('\$', ''));
+  }
+
+  // Calculate subtotal
+  double calculateSubtotal() {
+    double subtotal = 0.00;
+    for (final cartItem in myCart) {
+      double itemPrice = parsePrice(cartItem['price']);
+      int quantity = cartItem['quantity'];
+      subtotal += itemPrice * quantity;
+    }
+    return subtotal;
+  }
+
+  double calculateTotal() {
+    double deliveryCost = 2.00;
+    return calculateSubtotal() + deliveryCost;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
         children: [
@@ -39,7 +65,7 @@ class AddCard extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  width: 15,
+                  width: 10,
                 ),
                 Expanded(
                   child: CustomTextField(
@@ -50,6 +76,78 @@ class AddCard extends StatelessWidget {
               ],
             ),
           ),
+          const Spacer(),
+          Container(
+            color: AppDarkColors.black20,
+            height: 225,
+            width: double.infinity,
+            child: SizedBox(
+              width: width * 0.90,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 40, right: 40, bottom: 15, top: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Subtotal',
+                          style: CustomTextStyle12.h1Medium12,
+                        ),
+                        Text(
+                          '\$${calculateSubtotal().toStringAsFixed(2)}',
+                          style: CustomTextStyle14.h1SemiBold14,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, bottom: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Delivery',
+                          style: CustomTextStyle12.h1Medium12,
+                        ),
+                        Text(
+                          '\$2.00',
+                          style: CustomTextStyle14.h1SemiBold14,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, bottom: 40),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Total',
+                          style: CustomTextStyle12.h1Medium12,
+                        ),
+                        Text(
+                          '\$${calculateTotal().toStringAsFixed(2)}',
+                          style: CustomTextStyle14.h1SemiBold14,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: width * 0.90,
+                    child: CustomButton(
+                      buttonText: 'Proceed To checkout',
+                      onPressed: () {},
+                      buttonTextStyle: CustomTextStyle14.h1Medium14,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
