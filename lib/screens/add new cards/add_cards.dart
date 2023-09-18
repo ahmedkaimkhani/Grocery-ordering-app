ad497641d49c60b2d80e255569e4d21a886dac06
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-19 02:26:31 +0500

    Grocery

---
+++ lib/screens/add new cards/add_cards.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddCard extends StatelessWidget {
+  const AddCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
