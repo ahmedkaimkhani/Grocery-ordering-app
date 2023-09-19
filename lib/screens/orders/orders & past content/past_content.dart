94e171a2352cd19e9ad809909be29015e6394305
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-20 04:56:22 +0500

    Grocery

--- lib/screens/orders/orders & past content/past_content.dart
+++ lib/screens/orders/orders & past content/past_content.dart
@@ -9,7 +9,16 @@ class PastContent extends StatelessWidget {
       body: ListView.builder(
         itemBuilder: (context, index) {
           return Column(
-            children: [],
+            children: const [
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://w7.pngwing.com/pngs/1018/975/png-transparent-juice-banana-food-auglis-flavor-banana-natural-foods-leaf-orange-thumbnail.png'),
+                ),
+                title: Text('Bananas'),
+                subtitle: Text('\$55.50'),
+              )
+            ],
           );
         },
       ),
