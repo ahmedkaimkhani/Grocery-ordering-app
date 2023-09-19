25b92a4053a507ac9ddc9156cfdd6a6ac90b0a87
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-20 04:48:26 +0500

    Grocery

---
+++ lib/screens/orders/orders & past content/past_content.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PastContent extends StatelessWidget {
+  const PastContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Column(
+            children: [],
+          );
+        },
+      ),
+    );
+  }
+}
