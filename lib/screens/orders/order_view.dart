ce842e0f5819a85bd2d475d83e06641514e1820b
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-19 16:47:57 +0500

    Grocery

--- lib/screens/orders/order_view.dart
+++ lib/screens/orders/order_view.dart
@@ -3,10 +3,21 @@ import 'package:grocery_order_app_flutter/constants/app_colors.dart';
 import 'package:grocery_order_app_flutter/constants/custom_textstyle.dart';
 import 'package:grocery_order_app_flutter/widgets/custom_appbar.dart';
 
-class OrderView extends StatelessWidget {
-  OrderView({super.key});
+class OrderView extends StatefulWidget {
+  const OrderView({super.key});
 
-  bool isSelected = true;
+  @override
+  State<OrderView> createState() => _OrderViewState();
+}
+
+class _OrderViewState extends State<OrderView> {
+  int selectedTapIndex = 1;
+
+  void tappedButton(int index) {
+    setState(() {
+      selectedTapIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,37 +33,51 @@ class OrderView extends StatelessWidget {
               child: Row(
                 children: [
                   Expanded(
-                    child: Container(
-                      height: 45,
-                      decoration: BoxDecoration(
-                        color: AppColors.orange,
-                        borderRadius: BorderRadius.circular(60),
+                    child: InkWell(
+                      onTap: () => tappedButton(1),
+                      child: Container(
+                        height: 45,
+                        decoration: BoxDecoration(
+                          color: AppColors.orange,
+                          borderRadius: BorderRadius.circular(60),
+                        ),
+                        child: Center(
+                            child: Text(
+                          'Orders',
+                          style: selectedTapIndex == 1
+                              ? CustomTextStyle14.h1Medium14
+                              : CustomTextStyle14.h1Medium14Grey,
+                        )),
                       ),
-                      child: Center(
-                          child: Text(
-                        'Orders',
-                        style: CustomTextStyle14.h1Medium14,
-                      )),
                     ),
                   ),
                   const SizedBox(
                     width: 10,
                   ),
                   Expanded(
-                    child: Container(
-                      height: 45,
-                      decoration: BoxDecoration(
-                          color: AppDarkColors.black20,
-                          borderRadius: BorderRadius.circular(60)),
-                      child: Center(
-                          child: Text(
-                        'Past',
-                        style: CustomTextStyle14.h1Medium14Grey,
-                      )),
+                    child: InkWell(
+                      onTap: () => tappedButton(2),
+                      child: Container(
+                        height: 45,
+                        decoration: BoxDecoration(
+                            color: AppDarkColors.black20,
+                            borderRadius: BorderRadius.circular(60)),
+                        child: Center(
+                            child: Text(
+                          'Past',
+                          style: selectedTapIndex == 2
+                              ? CustomTextStyle14.h1Medium14
+                              : CustomTextStyle14.h1Medium14Grey,
+                        )),
+                      ),
                     ),
                   )
                 ],
               ),
+            ),
+            Container(
+              child:
+                  selectedTapIndex == 1 ? Text('Button One') : Text('Button 2'),
             )
           ],
         ),
