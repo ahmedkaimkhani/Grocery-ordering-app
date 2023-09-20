f272551e04872812dd110a1891e90e093d86639c
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-20 07:13:42 +0500

    Grocery

--- lib/screens/track orrder/track_order.dart
+++ lib/screens/track orrder/track_order.dart
@@ -20,57 +20,59 @@ class TrackOrder extends StatelessWidget {
               child: Column(
                 children: [
                   Expanded(
-                    child: Stack(
-                      children: [
-                        Container(
-                          height: 600,
-                          color: Colors.amber,
-                          child: Align(
-                            alignment: Alignment.topLeft,
-                            child: Image.asset('assets/images/map.jpg'),
+                    child: Container(
+                      height: 400,
+                      color: Colors.amber,
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Stack(children: [
+                          Image.asset(
+                            'assets/images/map.jpg',
+                            width: double.infinity,
+                            fit: BoxFit.fill,
                           ),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          left: 0,
-                          right: 0,
-                          child: Container(
-                            padding: const EdgeInsets.all(20.0),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(30),
-                              color: AppColors.orangeLite,
-                            ),
-                            child: ListTile(
-                              leading: const CircleAvatar(
-                                backgroundColor: Colors.blue,
-                                backgroundImage: NetworkImage(
-                                  'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg',
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: AppColors.orangeLite,
+                              ),
+                              child: ListTile(
+                                leading: const CircleAvatar(
+                                  backgroundColor: Colors.blue,
+                                  backgroundImage: NetworkImage(
+                                    'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg',
+                                  ),
+                                  radius: 24,
                                 ),
-                                radius: 24,
-                              ),
-                              title: Text(
-                                'Delivery Man',
-                                style: CustomTextStyle12.h1Regular12,
-                              ),
-                              subtitle: Text(
-                                'Rakibul Hassan',
-                                style: CustomTextStyle18.h1SemiBold18,
-                              ),
-                              trailing: Container(
-                                height: 48,
-                                width: 48,
-                                decoration: BoxDecoration(
-                                    color: AppColors.blue,
-                                    borderRadius: BorderRadius.circular(100)),
-                                child: Icon(
-                                  Icons.chat_outlined,
-                                  color: AppDarkColors.black1,
+                                title: Text(
+                                  'Delivery Man',
+                                  style: CustomTextStyle12.h1Regular12,
+                                ),
+                                subtitle: Text(
+                                  'Rakibul Hassan',
+                                  style: CustomTextStyle18.h1SemiBold18,
+                                ),
+                                trailing: Container(
+                                  height: 48,
+                                  width: 48,
+                                  decoration: BoxDecoration(
+                                      color: AppColors.blue,
+                                      borderRadius: BorderRadius.circular(100)),
+                                  child: Icon(
+                                    Icons.chat_outlined,
+                                    color: AppDarkColors.black1,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ]),
+                      ),
                     ),
                   ),
                 ],
