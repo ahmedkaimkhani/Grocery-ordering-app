12eec80b6ef67fee301799cee2e0007c9d46b737
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-30 04:39:52 +0500

    Grocery

--- lib/screens/add new cards/add_cards.dart
+++ lib/screens/add new cards/add_cards.dart
@@ -45,148 +45,146 @@ class AddCard extends StatelessWidget {
       resizeToAvoidBottomInset:
           false, //removed overflow erro when keyboard launches for input fields
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const CustomWhiteAppBar(
-                title: 'Add Card',
+        child: Column(
+          children: [
+            const CustomWhiteAppBar(
+              title: 'Add Card',
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 30, bottom: 40),
+              child: CustomTextField(
+                textName: 'Card holder name',
+                hintText: 'Card holder name',
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 30, bottom: 40),
-                child: CustomTextField(
-                  textName: 'Card holder name',
-                  hintText: 'Card holder name',
-                ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: CustomTextField(
+                textName: 'Card Number',
+                hintText: 'Card Number',
               ),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 40),
-                child: CustomTextField(
-                  textName: 'Card Number',
-                  hintText: 'Card Number',
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 40),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: CustomTextField(
-                        textName: 'Exp Date',
-                        hintText: 'Exp Date',
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: CustomTextField(
-                        textName: 'CVc',
-                        hintText: 'CVc Number',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 110,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 5, right: 5),
-                child: Container(
-                  height: 225,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: AppDarkColors.black10,
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30))),
-                  child: SizedBox(
-                    width: width * 0.90,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 40, right: 40, bottom: 15, top: 15),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Subtotal',
-                                style: CustomTextStyle12.h1Medium12,
-                              ),
-                              Text(
-                                '\$${calculateSubtotal().toStringAsFixed(2)}',
-                                style: CustomTextStyle14.h1SemiBold14,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 40, right: 40, bottom: 15),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Delivery',
-                                style: CustomTextStyle12.h1Medium12,
-                              ),
-                              Text(
-                                '\$2.00',
-                                style: CustomTextStyle14.h1SemiBold14,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 40, right: 40, bottom: 40),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Total',
-                                style: CustomTextStyle12.h1Medium12,
-                              ),
-                              Text(
-                                '\$${calculateTotal().toStringAsFixed(2)}',
-                                style: CustomTextStyle14.h1SemiBold14,
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          width: width * 0.90,
-                          child: CustomButton(
-                            buttonText: 'Proceed To checkout',
-                            onPressed: () {
-                              // Payment done Dialoge
-                              paymentDialoge(context);
-                              // add cart item to order list
-                              addCartToOrder();
-                              // clear cart item after adding into order list
-                              myCart.clear();
-                              Future.delayed(
-                                const Duration(seconds: 4),
-                                () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => const HomeView(),
-                                      ));
-                                },
-                              );
-                            },
-                            buttonTextStyle: CustomTextStyle14.h1Medium14,
-                          ),
-                        ),
-                      ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: CustomTextField(
+                      textName: 'Exp Date',
+                      hintText: 'Exp Date',
                     ),
                   ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: CustomTextField(
+                      textName: 'CVc',
+                      hintText: 'CVc Number',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(left: 5, right: 5),
+              child: Container(
+                height: 225,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: AppDarkColors.black10,
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30))),
+                child: SizedBox(
+                  width: width * 0.90,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 40, right: 40, bottom: 15, top: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Subtotal',
+                              style: CustomTextStyle12.h1Medium12,
+                            ),
+                            Text(
+                              '\$${calculateSubtotal().toStringAsFixed(2)}',
+                              style: CustomTextStyle14.h1SemiBold14,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 40, right: 40, bottom: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Delivery',
+                              style: CustomTextStyle12.h1Medium12,
+                            ),
+                            Text(
+                              '\$2.00',
+                              style: CustomTextStyle14.h1SemiBold14,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 40, right: 40, bottom: 40),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total',
+                              style: CustomTextStyle12.h1Medium12,
+                            ),
+                            Text(
+                              '\$${calculateTotal().toStringAsFixed(2)}',
+                              style: CustomTextStyle14.h1SemiBold14,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: width * 0.90,
+                        child: CustomButton(
+                          buttonText: 'Proceed To checkout',
+                          onPressed: () {
+                            // Payment done Dialoge
+                            paymentDialoge(context);
+                            // add cart item to order list
+                            addCartToOrder();
+                            // clear cart item after adding into order list
+                            myCart.clear();
+                            // Payment done Dialoge
+                            paymentDialoge(context);
+                            Future.delayed(
+                              const Duration(seconds: 4),
+                              () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const HomeView(),
+                                    ));
+                              },
+                            );
+                          },
+                          buttonTextStyle: CustomTextStyle14.h1Medium14,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
