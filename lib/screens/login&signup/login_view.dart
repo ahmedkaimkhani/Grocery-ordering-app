a17bccb58b8f6f7ffd7dd1e2e3f2e199d9c47e55
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-29 05:21:20 +0500

    Grocery

--- lib/screens/login&signup/login_view.dart
+++ lib/screens/login&signup/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:grocery_order_app_flutter/constants/app_colors.dart';
 import 'package:grocery_order_app_flutter/screens/login&signup/widgets/custom_textformfield.dart';
 
 class LoginView extends StatelessWidget {
@@ -8,17 +9,27 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: AppDarkColors.black1,
         body: SafeArea(
-      child: ListView(
-        children: [
-          Container(
-            height: 200,
-            width: 200,
-            child: SvgPicture.asset('assets/images/logo.svg'),
+          child: ListView(
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                child: SvgPicture.asset('assets/images/logo.svg'),
+              ),
+              CustomTextFormField(
+                title: 'Email',
+                icon: Icons.alternate_email_outlined,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              )
+            ],
           ),
-          CustomTextFormField()
-        ],
-      ),
-    ));
+        ));
   }
 }
--- lib/screens/login&signup/widgets/custom_textformfield.dart
+++ lib/screens/login&signup/widgets/custom_textformfield.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_order_app_flutter/constants/app_colors.dart';
 
 class CustomTextFormField extends StatefulWidget {
-  const CustomTextFormField({super.key});
+  final String title;
+  final IconData? icon;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  const CustomTextFormField({
+    Key? key,
+    required this.title,
+    this.icon,
+    this.controller,
+    required this.validator,
+  }) : super(key: key);
 
   @override
   State<CustomTextFormField> createState() => _CustomTextFormFieldState();
@@ -12,18 +23,23 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
   Widget build(BuildContext context) {
     return Form(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: TextFormField(
+          controller: widget.controller,
           decoration: InputDecoration(
-            hintText: 'email',
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            hintText: widget.title,
+            focusedBorder: InputBorder.none,
+            prefixIcon: Icon(
+              widget.icon,
+              color: AppColors.blue,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppDarkColors.black10),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppDarkColors.black10),
+            ),
           ),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please enter some text';
-            }
-            return null;
-          },
         ),
       ),
     );
