db1a2a4790262401df11c08e39a8a80911995ddb
ahmedkaimkhani <ak9302359@gmail.com>
2023-10-02 01:55:33 +0500

    Grocery

--- lib/screens/profile/user_profile.dart
+++ lib/screens/profile/user_profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_order_app_flutter/constants/app_colors.dart';
+import 'package:grocery_order_app_flutter/constants/custom_textstyle.dart';
 import 'package:grocery_order_app_flutter/widgets/custom_appbar.dart';
 
 class UserProfile extends StatelessWidget {
@@ -17,10 +18,11 @@ class UserProfile extends StatelessWidget {
             height: 20,
           ),
           Stack(
+            alignment: Alignment.center,
             children: [
               Container(
-                height: 150,
-                width: 150,
+                height: 120,
+                width: 120,
                 decoration:
                     BoxDecoration(borderRadius: BorderRadius.circular(100)),
                 child: const CircleAvatar(
@@ -45,7 +47,52 @@ class UserProfile extends StatelessWidget {
                 ),
               )
             ],
-          )
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Ahmed Raza',
+            style: CustomTextStyle20.h1Bold20,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 25, right: 25, top: 40, bottom: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Email:',
+                  style: CustomTextStyle18.h1Bold318,
+                ),
+                Text(
+                  'ahmed@gmail.com',
+                  style: CustomTextColor16.h1SemiBold16,
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Divider(),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 25, right: 25, top: 5, bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Phone No:',
+                  style: CustomTextStyle18.h1Bold318,
+                ),
+                Text(
+                  '0315-2116095',
+                  style: CustomTextColor16.h1SemiBold16,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
