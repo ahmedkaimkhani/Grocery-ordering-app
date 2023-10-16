b2e4db725c6a8cd4a8f67dca218cfac4e4d19a1b
ahmedkaimkhani <ak9302359@gmail.com>
2023-10-17 01:43:31 +0500

    Grocery

--- lib/screens/orders/orders & past content/orders_content.dart
+++ lib/screens/orders/orders & past content/orders_content.dart
@@ -10,124 +10,137 @@ class OrderContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: myOrder.length,
-        itemBuilder: (context, index) {
-          final cartItem = myOrder[index];
-          return Container(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 12, right: 12),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      // radius: 24,
-                      backgroundImage: NetworkImage(
-                        cartItem['image'],
-                      ),
-                    ),
-                    title: Text(
-                      cartItem['productname'],
-                      style: CustomTextStyle14.h1Medium14Grey,
-                    ),
-                    subtitle: Text(
-                      cartItem['price'],
-                      style: CustomTextStyle14.h1SemiBold14,
-                    ),
-                    trailing: Text(
-                      'ID: #765433',
-                      style: CustomTextStyle14.h1SemiBold14,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 25, right: 25),
-                  child: Row(
+    return myOrder.isEmpty
+        ? Column(
+            children: [
+              Container(
+                height: 300,
+                width: 300,
+                child: Image.asset('assets/images/emptyrider.gif'),
+              ),
+            ],
+          )
+        : Expanded(
+            child: ListView.builder(
+              itemCount: myOrder.length,
+              itemBuilder: (context, index) {
+                final cartItem = myOrder[index];
+                return Container(
+                  child: Column(
                     children: [
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Stack(
-                              children: [
-                                SizedBox(
-                                    width: 150,
-                                    height: 175,
-                                    child: Image.asset(
-                                        'assets/images/delivery.gif')),
-                                const Positioned(
-                                  bottom: 0,
-                                  left: 0,
-                                  right: 0,
-                                  child: Center(
-                                    child: CircleAvatar(
-                                      backgroundColor: Colors.blue,
-                                      backgroundImage: NetworkImage(
-                                          'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg'),
-                                      radius: 20,
-                                    ),
-                                  ),
-                                )
-                              ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 12, right: 12),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            // radius: 24,
+                            backgroundImage: NetworkImage(
+                              cartItem['image'],
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Meet our rider, ',
-                                  style: CustomTextStyle12.h1Regular12,
-                                ),
-                                Text(
-                                  'Rakib',
-                                  style: CustomTextStyle14.h1SemiBold14,
-                                ),
-                              ],
-                            ),
-                          ],
+                          ),
+                          title: Text(
+                            cartItem['productname'],
+                            style: CustomTextStyle14.h1Medium14Grey,
+                          ),
+                          subtitle: Text(
+                            cartItem['price'],
+                            style: CustomTextStyle14.h1SemiBold14,
+                          ),
+                          trailing: Text(
+                            'ID: #765433',
+                            style: CustomTextStyle14.h1SemiBold14,
+                          ),
                         ),
                       ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25, right: 25),
+                        child: Row(
                           children: [
-                            Text(
-                              'Your Product',
-                              style: CustomTextStyle20.h1Regular20,
-                            ),
-                            Text(
-                              'are on the way',
-                              style: CustomTextStyle20.h1SemiBold20,
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Stack(
+                                    children: [
+                                      SizedBox(
+                                          width: 150,
+                                          height: 175,
+                                          child: Image.asset(
+                                              'assets/images/delivery.gif')),
+                                      const Positioned(
+                                        bottom: 0,
+                                        left: 0,
+                                        right: 0,
+                                        child: Center(
+                                          child: CircleAvatar(
+                                            backgroundColor: Colors.blue,
+                                            backgroundImage: NetworkImage(
+                                                'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg'),
+                                            radius: 20,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'Meet our rider, ',
+                                        style: CustomTextStyle12.h1Regular12,
+                                      ),
+                                      Text(
+                                        'Rakib',
+                                        style: CustomTextStyle14.h1SemiBold14,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                             const SizedBox(
-                              height: 15,
+                              width: 15,
                             ),
-                            CustomButton(
-                              buttonText: 'Track Order',
-                              buttonTextStyle: CustomTextStyle14.h1Medium14,
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => const TrackOrder(),
-                                ));
-                              },
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Your Product',
+                                    style: CustomTextStyle20.h1Regular20,
+                                  ),
+                                  Text(
+                                    'are on the way',
+                                    style: CustomTextStyle20.h1SemiBold20,
+                                  ),
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  CustomButton(
+                                    buttonText: 'Track Order',
+                                    buttonTextStyle:
+                                        CustomTextStyle14.h1Medium14,
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(
+                                        builder: (context) =>
+                                            const TrackOrder(),
+                                      ));
+                                    },
+                                  )
+                                ],
+                              ),
                             )
                           ],
                         ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 25, right: 25, top: 20),
+                        child: Divider(),
                       )
                     ],
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 25, right: 25, top: 20),
-                  child: Divider(),
-                )
-              ],
+                );
+              },
             ),
           );
-        },
-      ),
-    );
   }
 }
